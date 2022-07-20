0b8d5116a5334c29e75d8bd54d2f857ac00eb1ff
Mandeep Singh <mdeep14293@gmail.com>
2022-07-20 11:30:59 +0530

    Initial Commit

---
+++ CropImg_With_Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CropImg_With_Camera
+//
+//  Created by Deep Baath on 20/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
